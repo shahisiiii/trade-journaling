2c959712afb2886ae8034fa83a124e4aad9705b6
jabeel shahis <shahis@exthgen.in>
2026-03-09 14:43:27 +0530

    load initial data

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+python manage.py loaddata data.json
